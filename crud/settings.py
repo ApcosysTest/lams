a16d35ad7d724f84198aca04efb2e8e353113c19
ApcosysTest <121083496+ApcosysTest@users.noreply.github.com>
2023-02-22 11:49:00 +0530

    Update settings.py

--- crud/settings.py
+++ crud/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 DATABASES = {  
     'default': {  
         'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': 'lames-db',  
+        'NAME': 'lams',  
         'USER':'postgres',  
         'PASSWORD':'Apcosys001',  
-        'HOST':'lames-db.c66eam8fjckg.ap-south-1.rds.amazonaws.com',  
+        'HOST':'db-lams.c66eam8fjckg.ap-south-1.rds.amazonaws.com',  
         'PORT':'5432'  
     } 
 }  
